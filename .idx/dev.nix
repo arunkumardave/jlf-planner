3c9acab7e5c5293c3281f3497e683e9f64f8d135
arunk8517 <arunk8517@gmail.com>
2026-01-14 08:01:30 +0000

    UI changes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,7 +3,7 @@
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  # Use https://search.nixos.org/packages to find packages  
   packages = [
     # pkgs.go
     # pkgs.python311
